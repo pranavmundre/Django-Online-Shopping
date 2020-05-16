ec0bed26eba5daa25c73b6bf80708927648f213f
Pranav Mundre <pmundre1@gmail.com>
2020-05-16 14:18:35 +0530

    Update file

--- OnlineShopping/pwa_settings.py
+++ OnlineShopping/pwa_settings.py
@@ -12,12 +12,36 @@ PWA_APP_DISPLAY = 'standalone'
 PWA_APP_SCOPE = '/' 
 PWA_APP_ORIENTATION = 'any' 
 PWA_APP_START_URL = '/' 
-PWA_APP_ICONS = [ { 'src': '/static/images/logo/150x150.png', 'sizes': '150x150' } ] 
-PWA_APP_ICONS_APPLE = [ { 'src': '/static/images/logo/150x150.png', 'sizes': '150x150' } ] 
+PWA_APP_ICONS = [ 
+		{ 
+			'src': '/static/images/logo/150x150.png', 
+			'sizes': '150x150' 
+		} 
+	] 
+PWA_APP_ICONS_APPLE = [
+		{
+			'src': '/static/images/logo/150x150.png', 
+			'sizes': '150x150' 
+		}
+	] 
+
 PWA_APP_SPLASH_SCREEN = [ { 
 	'src': '/static/images/logo/150x150.png', 
 	'media': '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)' 
 } ] 
+
+PWA_APP_ICONS_APPLE = [
+	   {
+		   'src': '/static/images/logo/150x150.png',
+		   'sizes': '160x160'
+	   }
+   ]
+PWA_APP_SPLASH_SCREEN = [
+	   {
+		   'src': '/static/images/logo/150x150.png',
+		   'media': '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)'
+	   }
+   ]
 PWA_APP_DIR = 'ltr' 
 PWA_APP_LANG = 'en-US'
 PWA_SERVICE_WORKER_PATH =  os.path.join(BASE_DIR, 'template/pwa/serviceworker.js')
--- OnlineShopping/settings.py
+++ OnlineShopping/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost','192.168.43.142']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','192.168.43.142','59bccb36.ngrok.io']
 
 # Add site url without add '/' slash at the end
 SITE_URL = config('SITE_URL')
@@ -85,18 +85,18 @@ WSGI_APPLICATION = 'OnlineShopping.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-	# 'default': {
-	#     'ENGINE': 'django.db.backends.sqlite3',
-	#     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-	# }
-	'default':{
-		'ENGINE': 'django.db.backends.postgresql',
-		'NAME':	config('DB_NAME'),
-		'USER' : config('DB_USER'),
-		'PASSWORD' : config('DB_PASSWORD'),
-		'HOST' : config('DB_HOST'),
-		'PORT' : config('DB_PORT'),
+	'default': {
+	    'ENGINE': 'django.db.backends.sqlite3',
+	    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 	}
+	# 'default':{
+	# 	'ENGINE': 'django.db.backends.postgresql',
+	# 	'NAME':	config('DB_NAME'),
+	# 	'USER' : config('DB_USER'),
+	# 	'PASSWORD' : config('DB_PASSWORD'),
+	# 	'HOST' : config('DB_HOST'),
+	# 	'PORT' : config('DB_PORT'),
+	# }
 }
 
 
